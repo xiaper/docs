1330d8c485876c1da0954b15f72de7c7b0645ebd
jackning <270580156@qq.com>
2019-03-06 08:14:57 +0800

    update

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
     repo: 'xiaper/docs',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
+    // repoLabel: '查看源码',
     // 以下为可选的编辑链接选项
     // 假如你的文档仓库和项目本身不在一个仓库：
     docsRepo: 'xiaper/docs',

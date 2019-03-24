b0f158404e1e7f9f097a638bedb2c4c226e5dcd0
jackning <270580156@qq.com>
2019-03-25 07:23:53 +0800

    update

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -72,6 +72,7 @@ module.exports = {
               '/guide/mp',
               '/guide/mini',
               '/guide/webrtc',
+              '/guide/flutter',
               '/guide/jmeter'
             ]
           },{

5ce2b8daf1243c5dcd93b2c5f55e541e4b28242e
jackning <270580156@qq.com>
2021-01-25 17:16:30 +0800

    update

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -169,6 +169,7 @@ module.exports = {
               // '/other/compare',
               '/other/reference',
               '/other/support',
+              '/other/apns',
               '/other/about',
               '/faq/'
             ]
